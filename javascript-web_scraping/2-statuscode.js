de92c803b5f8949f4593caa1dbeab0ccba3ea59b
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-24 16:52:16 -0500

    task 2 v1 error handling

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,10 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error.message);
+    return;
+  }
+
   console.log('code:', response.statusCode);
-}
-);
+});
