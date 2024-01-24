3d0bd081ab039f1f4e87edb94cc6e37a52b2f045
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-24 18:41:05 -0500

    task 4 V3

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,9 +9,10 @@ if (!apiUrl) {
   process.exit(1);
 }
 
-if (apiUrl !== 'https://swapi-api.hbtn.io/api/films/') {
-  console.error('Error: La ruta proporcionada no es válida. Debe ser https://swapi-api.hbtn.io/api/films/');
+if (apiUrl !== 'https://swapi-api.hbtn.io/api/films') {
+  console.error('Error');
   process.exit(1);
+}
 
 request(apiUrl, (error, response, body) => {
   if (error) {
