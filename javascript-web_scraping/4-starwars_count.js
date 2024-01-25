276b20a7f89d984f69af342c7e9a15543fbeb420
ricardovaldesv <ricardovaldes@icloud.com>
2024-01-24 19:02:22 -0500

    task 4 V9

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = 18;
+// const characterId = 18;
 
 if (!apiUrl) {
   console.error('Usage: node 4-starwars_count.js <API_URL>');
@@ -16,7 +16,8 @@ request(apiUrl, (error, response, body) => {
 
   const films = JSON.parse(body).results;
   const wedgeAntillesMovies = films.filter((film) =>
-    film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
+  //  film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
+    film.characters.some(characterUrl => characterUrl.match(/\/18\//))
   );
 
   console.log(wedgeAntillesMovies.length);
