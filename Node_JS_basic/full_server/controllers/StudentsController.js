2179aa45c2e70c5444e459f6d19443c7e77bed7c
ricardovaldesv <ricardovaldes@icloud.com>
2024-02-10 12:40:43 -0500

    task 8 v0

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,38 @@
+const { readDatabase } = require('../utils');
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const studentsByField = await readDatabase(process.argv[2]);
+      const response = ['This is the list of our students'];
+      let totalStudents = 0;
+
+      Object.keys(studentsByField).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).forEach((field) => {
+        totalStudents += studentsByField[field].length;
+        response.push(`Number of students in ${field}: ${studentsByField[field].length}. List: ${studentsByField[field].join(', ')}`);
+      });
+
+      // response.splice(1, 0, `Number of students: ${totalStudents}`);
+      res.status(200).send(response.join('\n'));
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+    if (major !== 'CS' && major !== 'SWE') {
+      return res.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const studentsByField = await readDatabase(process.argv[2]);
+      const studentsInMajor = studentsByField[major] || [];
+      res.status(200).send(`List: ${studentsInMajor.join(', ')}`);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+module.exports = StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises;
+
+async function readDatabase(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.trim().split('\n');
+    const fields = lines[0].trim().split(',');
+    const studentsByField = {};
+
+    lines.slice(1).forEach((line) => {
+      const values = line.trim().split(',');
+      const field = values[fields.indexOf('field')];
+      const firstname = values[fields.indexOf('firstname')];
+      
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
+      }
+      studentsByField[field].push(firstname);
+    });
+
+    return studentsByField;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = { readDatabase };
